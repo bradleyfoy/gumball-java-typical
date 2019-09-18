2c255827a6d579392bf269b8110932cbabba1d32
Brad Foy <48165889+bradleyfoy@users.noreply.github.com>
2019-09-17 21:48:29 -0700

    All basic requirements have been met
    The assignment as written is complete but no implementation of additional features such as:
    * Coin return not implemented
    * No information about how many gumballs remain

--- Gumball/src/gumball/Gumball.java
+++ Gumball/src/gumball/Gumball.java
@@ -17,15 +17,100 @@ public class Gumball {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GumballMachine myGumball = new GumballMachine(2);
+
         Scanner scanner = new Scanner(System.in);
-        int coinValue = 0;
-        while(coinValue != -1)
+        int machineType = 0;
+        OneQuarter myOneQuarter = new OneQuarter(2);
+        TwoQuarters myTwoQuarters = new TwoQuarters(2);
+        AllCoins myAllCoins = new AllCoins(20);
+        while(machineType != -1)
         {
-            System.out.println("Enter a coin value");
-            coinValue = scanner.nextInt();
-            myGumball.insertQuarter(coinValue);
-            myGumball.turnCrank();
+            System.out.println("Enter a machine type\n1. Single quarter machine \n2. Two quarter machine \n3. Multi coin machine\nEnter 1, 2, or 3");
+            machineType = scanner.nextInt();
+            if (machineType == 1)
+            {
+                myOneQuarter.insertCoins("Quarter");
+                myOneQuarter.turnCrank();
+                myOneQuarter.retrieve();
+            } else if (machineType == 2)
+            {
+                myTwoQuarters.insertCoins("Quarter");
+                myTwoQuarters.insertCoins("Quarter");
+                myTwoQuarters.turnCrank();
+                myTwoQuarters.retrieve();
+            } else if (machineType == 3)
+            {
+                myAllCoins.insertCoins("Nickle");
+                myAllCoins.insertCoins("Quarter");
+                myAllCoins.insertCoins("Dime");
+                myAllCoins.insertCoins("Dime");
+                myAllCoins.turnCrank();
+                myAllCoins.retrieve(); 
+            }
+            else if (machineType == 4)
+            {
+                myAllCoins.insertCoins("Quarter");
+                myAllCoins.insertCoins("Quarter");
+                myAllCoins.insertCoins("Dime");
+                myAllCoins.turnCrank();
+                myAllCoins.retrieve(); 
+            }
+            else if (machineType == 5)
+            {
+                myOneQuarter.insertCoins("Quarter");
+                myOneQuarter.turnCrank();
+                myOneQuarter.retrieve();
+            }
+            else if (machineType == 6)
+            {
+                myOneQuarter.insertCoins("Nickle");
+                myOneQuarter.turnCrank();
+                myOneQuarter.retrieve();
+            }
+            else if (machineType == 7)
+            {
+                myOneQuarter.insertCoins("Dime");
+                myOneQuarter.insertCoins("Dime");                
+                myOneQuarter.turnCrank();
+                myOneQuarter.retrieve();
+            }
+            else if (machineType == 8)
+            {
+                myOneQuarter.insertCoins("Quarter");
+                myOneQuarter.turnCrank();
+                myOneQuarter.retrieve();
+            }
+            else if (machineType == 9)
+            {
+                myTwoQuarters.insertCoins("Quarter");
+                myTwoQuarters.insertCoins("Penny");
+                myTwoQuarters.turnCrank();
+                myTwoQuarters.retrieve(); 
+            }
+            else if (machineType == 10)
+            {
+                myTwoQuarters.insertCoins("Quarter");
+                myTwoQuarters.turnCrank();
+                myTwoQuarters.retrieve();
+            }
+            else if (machineType == 11)
+            {
+                myTwoQuarters.insertCoins("Quarter");
+                myTwoQuarters.insertCoins("Quarter");
+                myTwoQuarters.turnCrank();
+                myTwoQuarters.retrieve();
+            }
+            else if (machineType == 12)
+            {
+                myAllCoins.insertCoins("Nickle");
+                myAllCoins.insertCoins("Quarter");
+                myAllCoins.insertCoins("Dime");
+                myAllCoins.insertCoins("Dime");
+                myAllCoins.turnCrank();
+                myAllCoins.retrieve(); 
+            }
+            
         }
+        scanner.close();
     }
 }
--- Gumball/src/gumball/GumballMachine.java
+++ Gumball/src/gumball/GumballMachine.java
@@ -4,50 +4,201 @@
  * and open the template in the editor.
  */
 package gumball;
-
+import java.util.Scanner;
 /**
  *
  * @author foy
  */
 public class GumballMachine
 {
-
-    private int num_gumballs;
-    private boolean has_quarter;
+    private int num_gumballs = 0;
+    private int nickleCount = 0;
+    private int dimeCount = 0;
+    private int quarterCount = 0;
+    private int totalPaid = 0;
+    private int numberToDispense = 0;
+    private String userMessage = "";
 
     public GumballMachine( int size )
     {
-        // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+    }
+    public int getNum_gumballs()
+    {
+        return this.num_gumballs;
+    }
+    public int getNickleCount()
+    {
+        return this.nickleCount;
+    } 
+    public int getDimeCount()
+    {
+        return this.dimeCount;
+    } 
+    public int getQuarterCount()
+    {
+        return this.quarterCount;
+    }
+    public int getTotalPaid()
+    {
+        return this.totalPaid;
     }
 
-    public void insertQuarter(int coin)
+    public int getNumberToDispense()
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+        return this.numberToDispense;
     }
-    
+    public String getUserMessage()
+    {
+        return this.userMessage;
+    }
+    public void setNickleCount(int myNickleCount)
+    {
+         this.nickleCount =  myNickleCount;
+    } 
+    public void setDimeCount(int myDimeCount)
+    {
+         this.dimeCount = myDimeCount;
+    } 
+    public void setQuarterCount(int myQuarterCount)
+    {
+         this.quarterCount = myQuarterCount;
+    }
+    public void setTotalPaid (int myTotalPaid)
+    {
+        this.totalPaid = myTotalPaid;
+    }
+    public void setUserMessage (String myUserMessage)
+    {
+        this.userMessage = myUserMessage;
+    }
+    public void setNum_gumballs(int myNum_gumballs)
+    {
+        this.num_gumballs = myNum_gumballs;
+    }
+    public void setNumberToDispense(int myNumberToDispense)
+    {
+        this.numberToDispense = myNumberToDispense;
+    }
+        
+    public void insertCoins(String coinType)
+    {
+        if (coinType.equals("Quarter"))
+        {
+            quarterCount +=1;
+        }else if (coinType.equals("Dime"))
+        {
+            dimeCount +=1;
+        }else if (coinType.equals("Nickle"))
+        {
+            nickleCount += 1;
+        }
+    }
+
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        if (getQuarterCount() + getDimeCount() + getNickleCount() == 0)
+        {
+            setUserMessage("Please insert money");
+            return;
+        }
+        if (( getNum_gumballs() - getNumberToDispense() < 0 ))
+        {
+            setUserMessage("No More Gumballs!  Sorry, can't return your money." );
+            return;
+        }
+        setTotalPaid(getNickleCount() * 5 + getDimeCount() * 10 + getQuarterCount() * 25);   
+    }
+    
+    public void dispense()
+    {
+        setNum_gumballs(getNum_gumballs() - getNumberToDispense());
+    
+    }    
+    public void reset()
+    {
+        setNickleCount(0);
+        setDimeCount(0);
+        setQuarterCount(0); 
+        setUserMessage("");
     }
 }
+
+class OneQuarter extends GumballMachine
+{
+    public OneQuarter( int size )
+    {
+        super(size);
+        this.setNumberToDispense(1);
+    }
+    public void retrieve()
+    {
+        if(getUserMessage().equals(""))
+        {
+            if (this.getTotalPaid() == 25 && this.getQuarterCount() == 1)
+            {
+                setUserMessage("Here's one gumball\n");
+                dispense();
+            }
+            else
+            {
+                setUserMessage("You paid " + getTotalPaid() + " cents but the cost is 25 cents");
+            }
+        }
+        System.out.println(getUserMessage());
+        reset();
+    }    
+}
+
+class TwoQuarters extends GumballMachine
+{
+    public TwoQuarters( int size )
+    {
+        super(size);
+        this.setNumberToDispense(2);
+    }
+    public void retrieve()
+    {
+        if(getUserMessage().equals(""))
+        {
+            if (this.getTotalPaid() == 50 && this.getQuarterCount() == 2)
+            {
+                setUserMessage("Here's two gumballs\n");
+                dispense();
+            }
+            else
+            {
+                setUserMessage("You paid " + getTotalPaid() + " cents but the cost is 50 cents");
+            }
+        }
+        System.out.println(getUserMessage());
+        reset();
+    } 
+}
+
+class AllCoins extends GumballMachine
+{
+    public AllCoins( int size )
+    {
+        super(size);
+        this.setNumberToDispense(5);
+    }
+    public void retrieve()
+    {
+        if(getUserMessage().equals(""))
+        {
+            if (this.getTotalPaid() == 50)
+            {
+                setUserMessage("Here's a bunch of Sweet Tarts\n");
+                dispense();
+            }
+            else
+            {
+                setUserMessage("You paid " + getTotalPaid() + " cents but the cost is 50 cents");
+            }
+        }
+        System.out.println(getUserMessage());
+        reset();
+    } 
+}
+
